eb672271952b49ad9af7583057129528faf48019
rusty <rusty@rustys-MacBook-Air.local>
2021-09-13 22:20:57 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 16 Solution
+ *  Copyright 2021 Rebecca Pina Partidas
+ */
+
+// Libraries
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        final int legalAge = 16;
+
+        // Determines if user is old enough to drive using age input
+        System.out.print("What is your age? ");
+        int age = input.nextInt();
+
+        if (age >= legalAge)
+        {
+            System.out.println("You are old enough to legally drive.");
+        }
+        else
+        {
+            System.out.println("You are not old enough to legally drive.");
+        }
+    }
+}
